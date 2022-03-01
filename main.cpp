133bb9865a7bd3c1c272e03cc01618e24c533dad
Miguel Basilio <lokoangel06@gmail.com>
2022-02-28 20:48:40 -0800

    check writer

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 
 #include <iostream>
 #include <fstream>
+#include<iomanip>
 #include <string>
-#include<vector>
 #include "input121.h"
 
 using namespace std;
@@ -14,8 +14,7 @@ using namespace std;
 int menuOption(); //prototype
 void Challenge11();
 void Challenge13();
-// Validating input:
-bool checkDate(string);
+
 // Determines the correct month name
 string months(int);
 // Inserts month, day and year into the date string.
@@ -23,10 +22,30 @@ void printDate(string, int, int);
 
 void Challenge15();
 void Challenge17();
+
 void Challenge19();
+char     tryAgain();
+void   getData(string&, string&, string&);
+void   checkRecipient(string&);
+void     checkAmount(string&);
+string removePunct(string&);
+string toHundred(const string*, const string*, int);
+string toThousand(string, const string*, int);
+string toTenThousand(string, const string*, int);
+string numToWord(string);
+string formatCheck(string, string&, string);
+void   displayCheck(string, const string, const string, const string);
+// Validating input:
+bool checkDate(string);
+
 
 int main() //driver*
 {
+
+
+    cout << "\tMiguel Basilio\n";
+    cout << "\tCMPR121" << endl;
+
     do
     {
         switch (menuOption())
@@ -101,7 +120,6 @@ void reverse(char* array)
 
     }
 }
-
 void Challenge11()
 {//document all functions:
 
@@ -124,10 +142,6 @@ void Challenge11()
     //dealocate array
     delete[] input;
 }
-
-
-
-
 void Challenge13()
 {
   
@@ -158,11 +172,6 @@ void Challenge13()
     date = months(month);
     printDate(date, day, year);
 }
-
-   
-
-
-
 bool checkDate(string date)
 {
     //validating date
@@ -221,8 +230,6 @@ bool checkDate(string date)
 
     return validDate;
 }
-
-
 //adding month to date:
 string months(const int month)
 {
@@ -233,8 +240,6 @@ string months(const int month)
 
     return monthNames[month - 1];
 }
-
-
 //prints date
 void printDate(string date, int day, int year)
 {
@@ -242,7 +247,6 @@ void printDate(string date, int day, int year)
 
     cout << "Date entered: " << date << endl;
 }
-
 void Challenge15()
 {
 
@@ -286,8 +290,6 @@ void Challenge15()
     cout << "Lower Case letter: " << lowerCases << endl;
     cout << "Digits counted: " << digits << endl;
 }
-   
-
 void Challenge17()
 {//assigning morse simbols 0-9, alphabet and morse conversion
     string digits[10] = { "-----", ".----","..---","...--","....-",".....","-....","--...","---..","----."};
@@ -349,10 +351,262 @@ void Challenge17()
     }
 }
 
-
 void Challenge19()
 {
     //reuse date format from challenge 13.
+    string amount;
+    string date;
+    string recipient;
+    string centAmount;
+    string wordAmount;
+    char   again = ' ';
+
+    do
+    {
+        amount.clear();
+        date.clear();
+        recipient.clear();
+        wordAmount.clear();
+        centAmount.clear();
+
+        getData(amount, date, recipient);
+        wordAmount = formatCheck(amount, wordAmount, centAmount);
+        displayCheck(amount, date, recipient, wordAmount);
+
+        again = tryAgain();
+
+        if (again == 'N')
+        {
+            cout << "\Thank you! Have a nice day!" << endl;
+        }
+
+    } while (again != 'N');
 
 
+}
+
+  // getting customers data:
+
+void getData(string& amount, string& date, string& recipient)
+{
+    cout << "\n\n\t\t\tCHECK WRITER\n\n"
+        << "\n\tPlease enter the recipient's name: ";
+    getline(cin, recipient);
+
+    while (recipient.empty())
+    {
+        cout << "\n\tDear customer, the name field must not be empty!" << endl;
+        cout << "\tPlease enter the recipient's name: ";
+        getline(cin, recipient);
+    }
+
+    checkRecipient(recipient);
+
+    cout << "\n\tPlease enter date: (Ex: 05/08/2017)" << endl;
+    getline(cin, date);
+    while (checkDate(date) == false)
+    {
+        if (checkDate(date) == false)
+        {
+            cout << "\n\tThe date is invalid try again.\n"
+                << "\n\tremember the format shoulbe mm/dd/yyyy\n"
+                << "\n\tPlease enter a date: " << endl;
+            getline(cin, date);
+        }
+    }
+
+    cout << "\n\tPlease enter amount: $";
+    getline(cin, amount);
+
+    checkAmount(amount);
+}
+
+//checks on the string object containing the recipient's name
+void checkRecipient(string& recipient)
+{
+    for (size_t index = 0; index < recipient.length(); index++)
+    {
+        while (!isalpha(recipient.at(0)) || ispunct(recipient[index]) ||
+            isdigit(recipient.at(index)) || isspace(recipient.at(0)))
+        {
+            cout << "\n\tField must not be empty, and it must not contain any numbers or punctuation marks!\n\n"
+                << "\n\tPlease enter the recipient's name: ";
+            getline(cin, recipient);
+        }
+    }
+}
+
+// Checks for validity of the entered amount. It must not be lower than 0.00 or higher than 9999.99.
+void checkAmount(string& amount)
+{
+    while (stod(amount) <= 0.00 || stod(amount) > 9999.99)
+    {
+        cout << "\n\tInput Failure! Dear customer, you entered an amount\n"
+            << "\tlower than 1 or greater than 9999.99. The maximum\n"
+            << "\tamount is $9999.99.\n\n"
+            << "\n\tPlease enter the desired amount:\t\t$";
+        getline(cin, amount);
+    }
+}
+
+//"converts" numbers between 1 and 9999.99 to words:
+string numToWord(string amount)
+{
+    const string oToTwenty[] = { "", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten",
+   "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen " };
+
+    const string tenMult[] = { "", "", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninenty " };
+
+    int     numStore = stoi(amount);
+    string numWord;
+    string words;
+
+    if (amount.length() < 3)
+    {
+        words = toHundred(oToTwenty, tenMult, numStore);
+    }
+    else if (amount.length() == 3)
+    {
+        words = toThousand(amount, oToTwenty, numStore) +
+            toHundred(oToTwenty, tenMult, numStore % 100);
+    }
+    else if (amount.length() == 4)
+    {
+        if (numStore % 1000 < 100)
+        {
+            words = toTenThousand(amount, oToTwenty, numStore) +
+                toHundred(oToTwenty, tenMult, numStore % 100);
+        }
+        else
+        {
+            words = toTenThousand(amount, oToTwenty, numStore) +
+                toThousand(amount, oToTwenty, numStore) +
+                toHundred(oToTwenty, tenMult, numStore % 100);
+        }
+    }
+
+    return numWord.append(words);
+}
+// to houndreds:
+string toHundred(const string* oToTwenty,
+    const string* tenMult, int numSize)
+{
+    string numStore;
+
+    numSize < 20 ? numStore.append(oToTwenty[numSize]) :
+        numStore.append(tenMult[numSize / 10] +
+            oToTwenty[numSize % 10]);
+
+    return numStore;
+}
+
+//to thousands:
+string toThousand(string amount, const string* oToTwenty, int numSize)
+{
+    string numStore;
+
+     /*This if/else statement covers all sums above and below
+        below 1000. Depending on which, the corresponding subscript
+        position is extracted and stored in numSize. numSize, after
+        performing a calculation on it, points to the appropriate
+        position in the string array containing the digit in word-
+        form. */
+    if (numSize > 999)
+    {
+        numSize = stoi(amount.erase(2, 3));
+        numStore.append(oToTwenty[numSize % 1000 % 10] + "hundred ");
+    }
+    else
+    {
+        numSize = stoi(amount.erase(1, 2));
+        numStore.append(oToTwenty[numSize] + "hundred ");
+    }
+
+    return numStore;
+}
+  //This function gets the index position of the "ten thousands"
+string toTenThousand(string amount, const string* oToTwenty, int numSize)
+{
+    string numStore;
+
+    numSize = stoi(amount.erase(1, 3));
+
+    return numStore.append(oToTwenty[numSize] + "thousand ");
+}
+
+string removePunct(string& amount)
+{
+    string centAmount;
+    //If the user enters an amount: "256.60", the cent amount is stored in a string object, and the '.' is erased.
+//If the user enters an amount: "256" without '.', '00' is appended to 'centAmount'.
+
+    for (size_t index = 0; index < amount.length(); index++)
+    {
+        if (ispunct(amount.at(index)))
+        {
+            centAmount.append(amount, index + 1, 3);
+            amount.erase(index, 3);
+        }
+    }
+    //If the user enters an amount: "256.2", a '0' is appended.
+    if (centAmount.empty())
+    {
+        centAmount.append("00");
+    }
+    else if (centAmount.length() == 1)
+    {
+        centAmount.append("0");
+    }
+
+    return centAmount;
+}
+
+
+//formats the check output
+string formatCheck(string amount, string& wordAmount, string centAmount)
+{
+    centAmount = removePunct(amount);
+    wordAmount = numToWord(amount);
+    //stores the result in a string object.The string object is returned.
+    wordAmount.append("dollar(s) and " + centAmount + " cents");
+    wordAmount.at(0) = toupper(wordAmount.at(0));
+
+    return wordAmount;
+}
+
+
+// This function outputs the formatted check to screen.
+void displayCheck(string amount, const string dueDate,
+    const string recipient, const string wordAmount)
+{
+    cout << fixed << showpoint << setprecision(2);
+    cout << "\n\t" << setw(68) << setfill('-') << "\n\n";
+    cout << setfill(' ') << "\n" << setw(64) << right
+        << "Date: " << dueDate << "\n"
+        << "\tPay to the order of: " << setw(28) << left << recipient
+        << setw(2) << right << "$" << stod(amount) << "\n\n\t"
+        << wordAmount << "\n\n\t"
+        << setw(31) << setfill('_') << "\n"
+        << "\n\n\t" << setw(68) << setfill('-') << "\n";
+}
+
+
+//asks the user if he or she wishes to write another check.
+char tryAgain()
+{
+    char again = ' ';
+
+    cout << "Do you wish to write another check? (y/N): " << endl;
+    cin >> again;
+    cin.ignore();
+
+    /* Input validation */
+    while (toupper(again) != 'Y' && toupper(again) != 'N')
+    {
+        cout << "Do you wish to write another check? (y/N): " << endl;
+        cin >> again;
+        cin.ignore();
+    }
+
+    return toupper(again);
 }
